709b2792c8247055a5ade88d01716703300cba51
Sfven <dat.dat82@gmail.com>
2024-02-17 12:49:29 -0600

    cleanup

--- main.gd
+++ main.gd
@@ -1,11 +1,8 @@
 @tool
 extends Node2D
 
-
 func _process(_delta):
 	queue_redraw()
-	
-	
 
 func _draw():
 	draw_rect(Rect2(0.0,0.0,1280,720),Color8(151,208,209,255))
@@ -13,4 +10,3 @@ func _draw():
 	draw_circle(Vector2(300,267.5), 17.5, Color8(166,94,53,255))
 	draw_rect(Rect2(50,250.0,30, 375),Color8(166,94,53,255))
 	draw_rect(Rect2(225,250.0,30, 410),Color8(166,94,53,255))
-	
